320471569eb9e72682f83d8eeec4582d148ff9b9
Upendra <usahu998@gmail.com>
2020-01-09 16:03:17 +0530

    Adding Locators Examples

--- src/main/java/com/selenium/Keyboard_Mouse_Operations.java
+++ src/main/java/com/selenium/Keyboard_Mouse_Operations.java
@@ -21,21 +21,21 @@ public class Keyboard_Mouse_Operations {
 //move the mouse by x and y coordinate
         r.mouseMove(300, 500);
 //press ALT key from keyboard
-        r.keyPress(KeyEvent.VK_ALT);
+     //   r.keyPress(KeyEvent.VK_ALT);
 //press F key from keyboard
         r.keyPress(KeyEvent.VK_F);
 //Release F key from keyboard
         r.keyRelease(KeyEvent.VK_F);
 //Release Alt key from keyboard
-        r.keyRelease(KeyEvent.VK_ALT);
+        //   r.keyRelease(KeyEvent.VK_ALT);
         Thread.sleep(3000);
 //Press W key from keyboard to open a new private window
-        r.keyPress(KeyEvent.VK_W);
+        //    r.keyPress(KeyEvent.VK_W);
 //Release W key from keyboard
-        r.keyRelease(KeyEvent.VK_W);
+        //    r.keyRelease(KeyEvent.VK_W);
         Thread.sleep(3000);
 // It will close only the current browser window
-//driver.close();
+        driver.close();
 // It will close all the browser windows opened by Selenium
         driver.quit();
     }
---
+++ src/main/java/com/selenium/Locators.java
@@ -0,0 +1,50 @@
+package com.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+    public static void main(String[] args) throws InterruptedException {
+        //setting the path of the chrome driver executable
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+
+        //Launch the Chrome browser
+        WebDriver driver = new ChromeDriver();
+
+        // Enter the URL of your own created sample web page
+        driver.get("https://github.com/login");
+
+        // Used “id” locator to find USERNAME text box
+        WebElement unTB = driver.findElement(By.id("login_field"));
+
+        //Clear the existing value present in the text box
+        unTB.clear();
+
+        // Enter value into the USERNAME text box
+        unTB.sendKeys("usahu998");
+
+        // Used “name” locator to find Password text box
+        WebElement passTB = driver.findElement(By.name("password"));
+
+        //Clear the existing value present in the text box
+        passTB.clear();
+
+        //Halt the program execution for 2 seconds
+        Thread.sleep(2000);
+
+        // Enter value into the Password text box
+        passTB.sendKeys("Upen@54321");
+
+        // Find the address of ActiTIME Link and click
+        driver.findElement(By.name("commit")).click();
+        Thread.sleep(2000);
+
+        // It will close only the current browser window
+        driver.close();
+
+        // It will close all the browser windows opened by Selenium
+        driver.quit();
+    }
+}
